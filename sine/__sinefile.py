73fd35f48ac1db3b099eb923c5e3e6808c5880e4
try-an <122729309+try-an@users.noreply.github.com>
2023-07-14 13:43:54 +0200

    modified some docstrings of the sinefile class

--- sine/__sinefile.py
+++ sine/__sinefile.py
@@ -4,53 +4,53 @@ from .__read_sine_file import read_sine_file
 class SineFile:
     """# `SineFile` object
 
-    ---
+---
 
-    A class that stores a sine file and it's informations.
+A class that stores a sine file and it's informations.
 
-    ---
-    
-    ## Properties
-    
-    `text_content`: the file's raw content
+---
 
-    `reapeat`: an int that represents how many times the file will repeat
+## Properties
 
-    `wave_type`: a string showing the type of wave that's being played
+`text_content`: the file's raw content
 
-    `sample_rate`: an int that represents the sample rate of the file
+`reapeat`: an int that represents how many times the file will repeat
 
-    `notes`: a list contaning tuples that represent frequency-duration pairs
+`wave_type`: a string showing the type of wave that's being played
 
-    `audio`: a `WaveForm` object containing the raw audio
+`sample_rate`: an int that represents the sample rate of the file
 
-    ## Methods
-    
-    `play`: play the file
-    
-    `save_to_wav`: save the audio to a .wav file
+`notes`: a list contaning tuples that represent frequency-duration pairs
 
-    `save_to_sine`: save the text content to a .sine file
-    
-    ## Constructor's arguments:
-    
-    `path`: the path to the sine file you want to make an object of"""
+`audio`: a `WaveForm` object containing the raw audio
+
+## Methods
+
+`play`: play the file
+
+`save_to_wav`: save the audio to a .wav file
+
+`save_to_sine`: save the text content to a .sine file
+
+## Constructor's arguments:
+
+`path`: the path to the sine file you want to make an object of"""
 
     def __init__(self, path: str):
         """Read a sine file from a file path and create a `SineFile` object,
-        letting you play and save the audio of the object (into a .wav or .sine file)
+letting you play and save the audio of the object (into a .wav or .sine file)
 
-        ---
+---
 
-        # Arguments
+# Arguments
 
-        `path`: just like it's name, it's the path
-        to the sine file
+`path`: just like it's name, it's the path
+to the sine file
 
-        ---
+---
 
-        For more about the `SineFile` object,
-        please see the docstring of the class or look at the documentation"""
+For more about the `SineFile` object,
+please see the docstring of the class or look at the documentation"""
 
         info = read_sine_file(path)
         self.text_content = info[0]
@@ -69,7 +69,7 @@ class SineFile:
         self.audio.save_audio(filename)
 
     def save_to_sine(self, filename: str):
-        """Save object to a .sine file"""
+        """Save the text content to a .sine file"""
         if filename[-5:] != ".sine":
             raise NameError("File must end in \".sine\"'")
 
