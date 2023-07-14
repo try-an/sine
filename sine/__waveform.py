392b66e34567488b594fd670e2dad45982589778
try-an <122729309+try-an@users.noreply.github.com>
2023-07-14 13:45:03 +0200

    modified the docstrings of the waveform class

--- sine/__waveform.py
+++ sine/__waveform.py
@@ -11,40 +11,59 @@ from .__waves import (
 class WaveForm:
     """# `WaveForm` object
 
-    ---
+---
 
-    A class that stores audio waves such as sine, square, and others,
-    having properties such as the sample rate and the
-    raw audio data, where you can save and play the audio.
+A class that stores audio waves such as sine, square, and others,
+having properties such as the sample rate and the
+raw audio data, where you can save and play the audio.
 
-    ---
-    
-    ## Properties
-    
-    `audio_data`: a numpy array containing floats representing the audio of the waveform
-    
-    `sample_rate`: an integer (usually 48 000 or 44 100)
-    representing the number of audio samples per second
-    
-    ## Methods
-    
-    `play_audio`: play the `audio_data` array
-    
-    `save_audio`: save the `audio_data` array to a .wav file
-    
-    ## Constructor's arguments:
-    
-    `wave_type`: either 'sine', 'square', 'saw', 'tri' or 'custom',
-    the arguments that follow (*args) are the informations about the wave
-    
-    `*args`: if the wave type is not 'custom', the arguments are:
-    frequency (in hz) and duration (in ms), if it is 'custom',
-    then the argument is just the audo data
-    
-    `sample_rate`: set's the sample rate attribute"""
+---
+
+## Properties
+
+`audio_data`: a numpy array containing floats representing the audio of the waveform
+
+`sample_rate`: an integer (usually 48 000 or 44 100)
+representing the number of audio samples per second
+
+## Methods
+
+`play_audio`: play the `audio_data` array
+
+`save_audio`: save the `audio_data` array to a .wav file
+
+## Constructor's arguments:
+
+`wave_type`: either 'sine', 'square', 'saw', 'tri' or 'custom',
+it's the waveform that is being played.
+
+`*args`: if the wave type is not 'custom', the arguments are:
+frequency (in hz) and duration (in ms), if it is 'custom',
+then the argument is just the audo data
+
+`sample_rate`: set's the sample rate attribute  (the sample rate is the number of samples played by second)"""
 
     def __init__(self, wave_type: str, *args, sample_rate: int):
-        """Basic class for wave forms"""
+        """Create the waveform object
+
+---
+
+# Arguments
+
+`wave_type`: either 'sine', 'square', 'saw', 'tri' or 'custom',
+it's the waveform that is being played.
+
+`*args`: if the wave type is not 'custom', the arguments are:
+frequency (in hz) and duration (in ms), if it is 'custom',
+then the argument is just the audo data
+
+`sample_rate`: set's the sample rate attribute  (the sample rate is the number of samples played by second)
+
+---
+
+For more about the `WaveForm` object,
+please see the docstring of the class or look at the documentation"""
+
         self.sample_rate = sample_rate
         self.audio_data = []
 
@@ -65,8 +84,10 @@ class WaveForm:
         self.audio_data = array(self.audio_data)
 
     def play_audio(self):
+        """Play the `audio_data` numpy array"""
         sd.play(self.audio_data, self.sample_rate, blocking=True)
         sd.stop()
 
     def save_audio(self, filename: str):
+        """Save the `audio_data` numpy array to a .wav file"""
         write(filename, self.sample_rate, self.audio_data)
